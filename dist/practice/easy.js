9de32577d764ae15dbc243cd057a0251a98ee777
gaojuphy <1571066810@qq.com>
2022-04-10 19:39:25 +0800

    去除dist

--- dist/practice/easy.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fooMyParameters = (arg1, arg2) => { };
-const barMyParameters = (arg1, arg2) => { };
-const bazMyParameters = () => { };
--- dist/projects/arithmetic.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const foo = (a, b) => a + b;
-const result = foo(1, 2); // => 3
